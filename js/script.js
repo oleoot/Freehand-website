10525de3ffa4ab20d7bf092b0bb511e53585ce65
oleoot <oleoot28@gmail.com>
2020-08-03 23:09:34 +0300

    Nav menu for mobile

--- js/script.js
+++ js/script.js
@@ -5,3 +5,18 @@ const reviewsSlider = tns({
     slideBy: 1,
     controlsContainer: '.main-slider__controls',
 });
+let btn = document.querySelector("#hidden-menu-btn");
+let li = document.querySelectorAll(".hidden-a")
+let menu = document.querySelector("#hidden-menu");
+btn.addEventListener("click", () => {
+    menu.classList.toggle("menu-active");
+    btn.classList.toggle("toggle")
+})
+
+
+li.forEach((item) => {
+    item.addEventListener("click", () => {
+        menu.classList.toggle("menu-active");
+        btn.classList.toggle("toggle")
+    })
+})
